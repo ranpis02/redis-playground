5e6096a1da48e1e0cd88b082b7f9b0974ca649dc
Zhihong Xie <ranpis02@gmail.com>
2026-06-18 13:16:21 +0800

    feat: add ShopController with REST annotations for shop endpoints

--- src/main/java/com/review/controller/ShopController.java
+++ src/main/java/com/review/controller/ShopController.java
@@ -1,4 +1,9 @@
 package com.review.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shop")
 public class ShopController {
 }
