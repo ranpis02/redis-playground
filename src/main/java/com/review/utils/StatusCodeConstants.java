e3cc26992ece7f87e62acf36a829f72054c7ad22
Zhihong Xie <ranpis02@gmail.com>
2026-06-18 13:12:48 +0800

    feat: add StatusCodeConstants class for standardized HTTP status codes

---
+++ src/main/java/com/review/utils/StatusCodeConstants.java
@@ -0,0 +1,11 @@
+package com.review.utils;
+
+public class StatusCodeConstants {
+    public static final int SUCCESS = 200;
+
+    public static final int UNAUTHORIZED = 401;
+
+    public static final int FORBIDDEN = 403;
+
+    public static final int NOT_FOUND = 404;
+}
