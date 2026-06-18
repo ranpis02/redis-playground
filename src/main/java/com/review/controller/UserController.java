dd51478b535267359dfe3ce6c49351461d635524
Zhihong Xie <ranpis02@gmail.com>
2026-06-18 13:12:22 +0800

    feat: add endpoints for sending verification code and user login in UserController

--- src/main/java/com/review/controller/UserController.java
+++ src/main/java/com/review/controller/UserController.java
@@ -1,9 +1,25 @@
 package com.review.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.review.model.dto.LoginFormDTO;
+import com.review.service.UserService;
+import com.review.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/code")
+    public R sendCode(@RequestParam("phone") String phone) {
+        return userService.sendCode(phone);
+    }
+
+    @PostMapping("/login")
+    public R login(@RequestBody LoginFormDTO loginForm) {
+        return userService.login(loginForm);
+    }
+
 }
