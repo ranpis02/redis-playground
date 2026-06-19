8025c85091308cab0e27b8d8aca048e63c68ce30
Zhihong Xie <ranpis02@gmail.com>
2026-06-19 23:42:21 +0800

    feat: add cache and lock key prefixes with TTL constants in RedisConstants

--- src/main/java/com/review/utils/RedisConstants.java
+++ src/main/java/com/review/utils/RedisConstants.java
@@ -16,7 +16,37 @@ public class RedisConstants {
     public static final String LOGIN_USER_PREFIX = "login:user:";
 
     /**
-     * Login user info TTL in seconds
+     * Login user info TTL in milliseconds
      */
     public static final Long LOGIN_USER_TTL = 36000L;
+
+    /**
+     * Prefix for shop cache keys
+     */
+    public static final String CACHE_SHOP_KEY_PREFIX = "cache:shop:";
+
+    /**
+     * Shop cache TTL in seconds
+     */
+    public static final Long CACHE_SHOP_TTL = 10L;
+
+    /**
+     * Blank cache TTL in seconds
+     */
+    public static final Long CACHE_BLANK_TTL = 10L;
+
+    /**
+     * Prefix for lock shop keys
+     */
+    public static final String LOCK_SHOP_KEY_PREFIX = "lock:shop:";
+
+    /**
+     * General prefix for lock keys
+     */
+    public static final String LOCK_KEY_PREFIX = "lock:";
+
+    /**
+     * Lock TTL in seconds
+     */
+    public static final Long LOCK_TTL = 10L;
 }
