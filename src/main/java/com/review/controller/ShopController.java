3012af7cad4f414e498d6d5831e083fd1ddfb439
Zhihong Xie <ranpis02@gmail.com>
2026-06-19 23:43:11 +0800

    feat: add queryById endpoint in ShopController to retrieve shop details

--- src/main/java/com/review/controller/ShopController.java
+++ src/main/java/com/review/controller/ShopController.java
@@ -1,9 +1,19 @@
 package com.review.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.review.service.ShopService;
+import com.review.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/shop")
 public class ShopController {
+
+    @Autowired
+    private ShopService shopService;
+
+    @GetMapping("/query")
+    public R queryById(@RequestParam Long id) {
+        return shopService.queryById(id);
+    }
 }
