24b2a9eaed5167e7b8cfd9d93724f4f3790397a1
Zhihong Xie <ranpis02@gmail.com>
2026-06-21 20:34:39 +0800

    refactor: replace RedisProperties injection with @Value annotations in RedissonConfig

--- src/main/java/com/review/config/RedissonConfig.java
+++ src/main/java/com/review/config/RedissonConfig.java
@@ -3,7 +3,7 @@ package com.review.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
@@ -14,20 +14,33 @@ import org.springframework.util.StringUtils;
  */
 @Configuration
 public class RedissonConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
 
-  @Bean(destroyMethod = "shutdown")
-  public RedissonClient redissonClient(RedisProperties redisProperties) {
-    Config config = new Config();
-    String address = "redis://" + redisProperties.getHost() + ":" + redisProperties.getPort();
+    @Value("${spring.data.redis.port}")
+    private int port;
 
-    config.useSingleServer()
-        .setAddress(address)
-        .setDatabase(redisProperties.getDatabase());
+    @Value("${spring.data.redis.database}")
+    private int databaseNo;
 
-    if (StringUtils.hasText(redisProperties.getPassword())) {
-      config.useSingleServer().setPassword(redisProperties.getPassword());
+    // If the password does exist, then set it to empty string
+    @Value("${spring.data.redis.password:}")
+    private String password;
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+
+        String address = "redis://" + host + ":" + port;
+
+        config.useSingleServer()
+                .setAddress(address)
+                .setDatabase(databaseNo);
+
+        if (StringUtils.hasText(password)) {
+            config.setPassword(password);
+        }
+
+        return Redisson.create(config);
     }
-
-    return Redisson.create(config);
-  }
 }
