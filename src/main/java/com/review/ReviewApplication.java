a3506c962d289c4683f6e71ef2945da197380088
Zhihong Xie <ranpis02@gmail.com>
2026-06-21 20:34:52 +0800

    feat: Enable AspectJ auto-proxy with exposed proxy

--- src/main/java/com/review/ReviewApplication.java
+++ src/main/java/com/review/ReviewApplication.java
@@ -3,12 +3,14 @@ package com.review;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * Spring Boot ReviewApplication 启动类
  */
 @SpringBootApplication
 @MapperScan("com.review.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class ReviewApplication {
 
     public static void main(String[] args) {
