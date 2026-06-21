c96544afe8e5a8050182360e0c083c84f45114c1
Zhihong Xie <ranpis02@gmail.com>
2026-06-21 12:51:08 +0800

    feat: return JSON error response for unauthorized requests

--- src/main/java/com/review/Interceptor/LoginInterceptor.java
+++ src/main/java/com/review/Interceptor/LoginInterceptor.java
@@ -1,41 +1,32 @@
 package com.review.Interceptor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.review.utils.R;
 import com.review.utils.UserHolder;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.nio.charset.StandardCharsets;
+
 import static com.review.utils.StatusCodeConstants.*;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
-    @Override
-    public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
-        // If user is not logged in, reject with 401
-        if(UserHolder.get() == null) {
-            response.setStatus(UNAUTHORIZED);
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
+    @Override
+    public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) throws Exception {
+        if (UserHolder.get() == null) {
+            response.setStatus(UNAUTHORIZED);
+            response.setContentType("application/json;charset=utf-8");
+            response.getOutputStream()
+                    .write(MAPPER.writeValueAsString(R.fail("Unauthorized")).getBytes(StandardCharsets.UTF_8));
             return false;
         }
 
-        // If user is logged in, then proceed
         return true;
-
-
-        // HttpSession session = request.getSession();
-
-        // // Get the user in the session
-        // Object user = session.getAttribute("user");
-        //
-        // // If User does not exist, then intercepts
-        // if(user == null) {
-        //     response.setStatus(401); // Unauthorized
-        //     return false;
-        // }
-        //
-        // // If user exists, then put the user into ThreadLocal for later use
-        // UserHolder.set((UserDTO) user);
-        // return true;
     }
 
 }
