df3db959b8df207810420644c1e821c31881ae6b
Zhihong Xie <ranpis02@gmail.com>
2026-06-21 20:37:06 +0800

    feat: add seckill voucher endpoints with naive and one-per-user implementations

--- src/main/java/com/review/controller/VoucherOrderController.java
+++ src/main/java/com/review/controller/VoucherOrderController.java
@@ -1,10 +1,28 @@
 package com.review.controller;
 
+import com.review.service.VoucherOrderService;
+import com.review.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/voucher-order")
 public class VoucherOrderController {
+    @Autowired
+    private VoucherOrderService voucherOrderService;
+
+    @PostMapping("/grab")
+    public R grabSeckillVoucher(@RequestParam Long id) {
+        return voucherOrderService.grabSeckillVoucherNaive(id);
+    }
+
+    @PostMapping("/grab/one")
+    public R grabSeckillVoucherOne(@RequestParam Long id) {
+        return voucherOrderService.grabSeckillVoucherOneRestriction(id);
+    }
+
 
 }
--- src/main/java/com/review/service/VoucherOrderService.java
+++ src/main/java/com/review/service/VoucherOrderService.java
@@ -2,6 +2,25 @@ package com.review.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.review.model.entity.VoucherOrder;
+import com.review.utils.R;
 
 public interface VoucherOrderService extends IService<VoucherOrder> {
+
+    /**
+     * Naive seckill implementation without any concurrency optimization.
+     * Serves as a baseline for comparing with optimized versions.
+     *
+     * @param id voucher ID
+     * @return operation result
+     */
+    R grabSeckillVoucherNaive(Long id);
+
+    /**
+     * Grab the seckill voucher with one-per-user restriction.
+     *
+     * @param id voucher ID
+     * @return operation result
+     */
+    R grabSeckillVoucherOneRestriction(Long id);
+
 }
