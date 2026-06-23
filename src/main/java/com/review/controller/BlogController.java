c1dd988991217e59bc5e996a64516c81fa350b2a
Zhihong Xie <ranpis02@gmail.com>
2026-06-23 19:49:41 +0800

    feat(controller): add blog query, like, and top likes endpoints

--- src/main/java/com/review/controller/BlogController.java
+++ src/main/java/com/review/controller/BlogController.java
@@ -1,10 +1,33 @@
 package com.review.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.review.service.BlogService;
+import com.review.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/blog")
 public class BlogController {
+    @Autowired
+    private BlogService blogService;
 
+    @GetMapping("/query")
+    public R queryBlog(@RequestParam Long id) {
+        return blogService.queryBlogById(id);
+    }
+
+    @GetMapping("/query-page")
+    public R queryHotBlog(@RequestParam Integer pageNo) {
+        return blogService.queryHotBlog(pageNo);
+    }
+
+    @PostMapping("/like")
+    public R likeBlog(@RequestParam Long id) {
+        return blogService.likeBlog(id);
+    }
+
+    @GetMapping("/query-likes-top5")
+    public R queryTop5(@RequestParam Long id) {
+        return blogService.queryBlogLikesTop5(id);
+    }
 }
